985b605fcf6b3725057e97e839c6156a3de4acd6
fiftie <kumavichi.t@gmail.com>
2022-01-21 14:24:20 +0900

    Initial Commit

---
+++ reversi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  reversi
+//
+//  Created by Tatsuyoshi Igarashi on 2022/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
